d692dfe0b588709217f92094df0bd5b8e61940ac
Nova Lautrup <marcusanholt@gmail.com>
2025-02-20 17:50:27 +0100

    Remove Minecart sonic booms
    I thought they'd be funny, but they trigger too often when you change directions :war:

--- common/src/main/java/org/theplaceholder/sonicboom/mixin/PlayerMixin.java
+++ common/src/main/java/org/theplaceholder/sonicboom/mixin/PlayerMixin.java
@@ -34,7 +34,7 @@ public abstract class PlayerMixin extends LivingEntity {
     @Inject(at = @At("HEAD"), method = "tick()V")
     public void tick(CallbackInfo info) {
         if(!this.level().isClientSide) return;
-        if(!this.isFallFlying() && (this.getVehicle() == null || this.getVehicle().getType() != EntityType.MINECART)) return;
+        if(!this.isFallFlying()) return;
 
         if(sonicBoom$lastPos == null) sonicBoom$lastPos = this.position();
         if(sonicBoom$getSpeed() > EXPLOSION_SPEED * (sonicBoom$sonicLevel + 1)){
